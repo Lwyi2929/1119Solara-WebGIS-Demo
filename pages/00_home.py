220218b3a1be9157a90e98fba7bde380ecc110ff
Lwyi2929 <lwyi2929@gmail.com>
2025-11-19 03:06:01 +0000

    modified:   pages/00_home.py 	new file:   "static/\351\246\254\345\244\252\351\236\215\346\272\252\347\251\272\346\213\215.png"

--- pages/00_home.py
+++ pages/00_home.py
@@ -1,1 +1,14 @@
 import solara
+
+
+@solara.component
+def Page():
+    solara.Title("我的 Solara WebGIS App")
+    ![馬太鞍溪空拍](https://raw.githubusercontent.com/Lwyi2929/1119Solara-WebGIS-Demo/main/static/馬太鞍溪空拍.png)
+
+    solara.Markdown("馬太鞍溪堰塞湖 事件於 2025 年 9 月 23 日 下午約 2 點 50 分 發生壩頂溢流，指因豪雨帶來的暴漲溪水，造成馬太鞍溪河岸堤防出現破口，導致周邊低窪地區淹水。潰堤主要與短時間內大量降雨、上游集水區排水不及，以及堤防老化或結構受損等因素有關。事故發生後，相關單位進行緊急搶修與疏散，並加強後續堤防的安全檢查與整治。")
+
+
+
+
+    
